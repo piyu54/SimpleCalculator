e4c23957b6493d4f4172bf29e75dcf5624f0b8d5
priya kushwaha <priyakushwaha1407@gmail.com>
2024-01-30 16:14:23 +0530

    Simple Calculator Project added

---
+++ src/com/calculator/Demo.java
@@ -0,0 +1,40 @@
+package com.calculator;
+
+import java.util.Scanner;
+
+public class Demo 
+{
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println(" \n 1.Addition \n 2.Substration \n 3.Multiplication \n 4.Division ");
+		System.out.println("ENTER YOUR CHOICE" );
+		String sys = s.next();
+		System.out.println("ENTER THE FIRST NUMBER");
+		int no1 = s.nextInt();
+		System.out.println("ENTER THE SECOND NUMBER");
+		int no2 = s.nextInt();
+		int res;
+		
+		switch (sys) 
+		{
+		case "1" : res = no1+no2;
+			System.out.println("Addition of Two Number is " +res);
+			break;
+		case "2" : res = no1-no2;
+		System.out.println("Substration of Two Number is "+res);
+		break;
+		case "3" : res = no1*no2;
+		System.out.println("Multiplication of Two Number is " +res);
+		break;
+		case "4" : res = no1/no2;
+		System.out.println("Division of Two Number is " +res);
+		break;
+		default :
+			System.out.println("Your Choice is Invaild Please Enter Right Choise");
+		}
+		
+	}
+}
